cff521ed29d2cd161dbfc6144c43fb937c7bda31
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-08-21 20:11:31 -0300

    enviando por linhade comando

--- codigos/menu_Rogue.py
+++ codigos/menu_Rogue.py
@@ -3,10 +3,8 @@ import game
 import os
 import console as cls
 
-cls.clear()
 
 def exibirmenu():
-    cls.clear()
     print("\tAqui vem a splash\n")
     print('1. Jogar')
     print('2. Configurações')
@@ -16,33 +14,44 @@ def exibirmenu():
 
 def escolhamenu():
     while True:
-        exibirmenu()
+        
         try:
             opcao = int(input('Digite a sua opção:'))
 
             if opcao == 1:
+                cls.clear()
                 print('Jogar')
                 game.inicia()
                 break
             elif opcao == 2:
+                cls.clear()
                 print('Configurações')
                 break
             elif opcao == 3:
+                cls.clear()
                 print('Progresso')
                 break
             elif opcao == 4:
+                cls.clear()
                 print('Ranking')
                 # call da função de ranking no arquivo correto
                 rk.exibir()
                 break
             elif opcao == 5:
+                cls.clear()
                 print('Sair')
                 break
             else:
+                cls.clear()
+                exibirmenu()
                 print("Opção inválida. Tente novamente.")
+
         except ValueError:
+            cls.clear()
+            exibirmenu()
             print("Digite um número válido. Tente novamente.")
 
 #Chamadas
-#exibirmenu()
+cls.clear()
+exibirmenu()    # precisa de ua primeira chamada
 escolhamenu()
