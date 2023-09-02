d864e99db5fe95931d99853b407ef96810cad1dc
Rodrigo <rodrigo.campos.mim@gmail.com>
2023-09-02 18:51:23 -0300

    Update Splash.py
    fiz uma splash básica

---
+++ codigos/Splash.py
@@ -0,0 +1,26 @@
+def splashscreen():  
+
+    largura = 50
+
+    def borda():
+        print('+' + '-' * (largura-2) + '+')
+
+    def splashcentralizada(texto):
+        espaço_total = largura - 2 - len(texto)
+        espaço_esquerda = espaço_total // 2
+        espaço_direita = espaço_total - espaço_esquerda
+        print('|' + ' ' * espaço_esquerda + texto + ' ' * espaço_direita + '|')
+
+    borda()
+    splashcentralizada('BEM VINDO AO INFERNO!')
+    splashcentralizada('')
+    splashcentralizada('')
+    splashcentralizada('PRESSIONE ENTER PARA COMEÇAR')
+    splashcentralizada('')
+    splashcentralizada('')
+    splashcentralizada('BOA SORTE, AVENTUREIRO.') 
+    borda()
+
+splashscreen()
+
+# Splashscreen básica e inicial, podemos pensar mais sobre ela depois #
