040b91bf304782434b5fe1efbe9bf116501cc28f
Rodrigo <rodrigo.campos.mim@gmail.com>
2023-09-02 19:24:07 -0300

    Update configuracoes.py

--- codigos/configuracoes.py
+++ codigos/configuracoes.py
@@ -1,1 +1,15 @@
-# Estavamos pensando em adicionar três níveis de dificuldade, o volume dá música (podendo o jogador desativar a música)
+class Configuracao:
+    def Configuraçoes_padroes(Padrão):
+        Padrão.dificuldade = 'médio'  
+        Padrão.resolução = '800x600'  
+        Padrão.volume_música = 50  
+        Padrão.música_habilitada = True  
+
+    def mostrar_configuracoes(Padrão):
+        print("Configurações do Jogo:")
+        print(f"Dificuldade: {Padrão.dificuldade}")
+        print(f"Resolução: {Padrão.resolução}")
+        print(f"Volume da Música: {Padrão.volume_música}")
+        print(f"Música: {'Habilitada' if Padrão.música_habilitada else 'Desabilitada'}")
+
+    # (Padrão) pode ser substituido por (self), comumente utilizado em Python. #
