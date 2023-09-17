97ef73d080c6ce7357f1de88d59ee3c7ebf59d4a
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-09-17 19:52:51 -0300

    estudando o curses

--- codigos/game.py
+++ codigos/game.py
@@ -1,10 +1,36 @@
-import curses
 import mapas as mp
 import console as cls
 import config as cf
+import curses
+import random
 
 def inicia():
     cls.clear()
-    mp.printMap(mp.mapa1)
-    a = input("Pressione Enter para summonar...")
+    curses.wrapper(movimento)
 
+def movimento(stdscr):
+    curses.curs_set(0) 
+    stdscr.nodelay(1) 
+
+    y, x = curses.LINES//2, curses.COLS//2
+    stdscr.addch(y, x, cf.player[0]) #dados armazenados na lista de ados do jogador
+    
+    while True:
+        key = stdscr.getch()
+    
+        if key == curses.KEY_UP:
+            y = max(y-1, 0)
+        elif key == curses.KEY_DOWN:
+            y = min(y+1, curses.LINES-1)
+        elif key == curses.KEY_LEFT:
+            x = max(x-1, 0)
+        elif key == curses.KEY_RIGHT:
+            x = min(x+1, curses.COLS-1)
+
+        stdscr.clear()
+        stdscr.addch(y, x, cf.player[0])
+        stdscr.refresh()
+
+
+#mp.printMap(mp.mapa1)
+#print(cf.player[0])
